8477ce93cc03131c6a2f235bef80a9a5ff8461f3
jeroenvw81 <jeroenvw81@gmail.com>
2025-11-10 11:06:58 +0100

    F: added split_node_image and link

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -2,6 +2,10 @@ import unittest
 
 from inline_markdown import (
     split_nodes_delimiter,
+    extract_markdown_images,
+    extract_markdown_links,
+    split_nodes_image,
+    split_nodes_links,
 )
 from textnode import TextNode, TextType
 
@@ -54,6 +58,70 @@ class Test_inline_markdown(unittest.TestCase):
             TextNode(" word", TextType.TEXT),
         ],
         new_nodes,
+        )
+
+    def test_extract_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/image.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/image.png")], matches
+        )
+
+    def test_extract_links(self):
+        matches = extract_markdown_links(
+            "This is text with a [link](https://boot.dev) and a [second link](https://https://github.com/Jeroenvw81/Static-Site)"
+        )
+        self.assertListEqual(
+            [
+                ("link", "https://boot.dev"),
+                ("second link", "https://https://github.com/Jeroenvw81/Static-Site"),
+                ],
+                matches,
+        )
+    
+    def test_extract_markdown_images(self):
+        node = TextNode("This is text with an ![image](https://i.imgur.com/image.png) and another ![second image](https://i.imgur.com/other_image.png)", TextType.TEXT)
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/image.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode("second image", TextType.IMAGE, "https://i.imgur.com/other_image.png"),
+            ],
+            new_nodes,
+        )
+
+    def test_extract_markdown_links(self):
+        node = TextNode(
+            "This is text with a link to [youtube](https://www.youtube.com)",
+            TextType.TEXT,
+            )
+        new_nodes = split_nodes_links([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a link to ", TextType.TEXT),
+                TextNode("youtube", TextType.LINK, "https://www.youtube.com")
+            ],
+            new_nodes
+        )
+
+    def test_extract_markdown_three_links(self):
+        node = TextNode(
+            "This is text with links to [youtube](https://youtube.com), [bootdev](https://boot.dev) and a third link to [google](https://google.com)",
+            TextType.TEXT,
+            )
+        new_nodes = split_nodes_links([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with links to ", TextType.TEXT),
+                TextNode("youtube", TextType.LINK, "https://youtube.com"),
+                TextNode(", ", TextType.TEXT),
+                TextNode("bootdev", TextType.LINK, "https://boot.dev"),
+                TextNode(" and a third link to ", TextType.TEXT),
+                TextNode("google", TextType.LINK, "https://google.com")
+            ],
+            new_nodes
         )
 
 if __name__ == "__main__":
